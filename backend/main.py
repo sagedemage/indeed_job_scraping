a4a41ff4aeea7d868fefa0062fc0e7a06d308f43
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-14 14:31:11 -0400

    Return rows of data for the /job-data api route

--- backend/main.py
+++ backend/main.py
@@ -19,28 +19,8 @@ def job_data():
     # Replace NaN values with an empty string
     df = df.fillna("")
 
-    job_titles_series = df["Job_Title"]
-    companies_series = df["Company"]
-    locations_series  = df["Location"]
-    salary_amounts_series  = df["Salary_Amount"]
-    salary_types_series  = df["Salary_Type"]
-    job_types_series  = df["Job_Type"]
-    links_series  = df["Link"]
+    rows = []
+    for i in range(len(df.index)):
+        rows.append(df.loc[i])
 
-    job_titles = job_titles_series.to_list()
-    companies = companies_series.to_list()
-    locations = locations_series.to_list()
-    salary_amounts = salary_amounts_series.to_list()
-    salary_types = salary_types_series.to_list()
-    job_types = job_types_series.to_list()
-    links = links_series.to_list()
-
-    return {
-        "job_titles": job_titles,
-        "companies": companies,
-        "locations": locations,
-        "salary_amounts": salary_amounts,
-        "salary_types": salary_types,
-        "job_types": job_types,
-        "links": links
-    }
+    return { "rows": rows }
