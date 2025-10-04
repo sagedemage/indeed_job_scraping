c42e8ac9c199787cbed6b7c71b6a43943e4e2646
Salmaan Saeed <salsaeed999@gmail.com>
2025-10-03 22:39:20 -0400

    setup the html templates for the flask application

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 """Backend Server for serving job data"""
 
 from flask import Flask
+from flask import render_template, url_for
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 csv_file = "data/indeed_jobs.csv"
 
 
-@app.get("/")
+@app.get("/hello")
 def read_root():
     return {"msg": "Hello World!"}
 
@@ -25,3 +26,15 @@ def job_data():
         rows.append(df.loc[i].to_dict())
 
     return {"rows": rows}
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/jobs")
+def jobs():
+    return render_template("jobs.html", title="Indeed Jobs", jobs_data=["Frist Job", "Second Job"])
